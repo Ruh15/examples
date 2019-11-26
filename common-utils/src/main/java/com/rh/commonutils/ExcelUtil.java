abea54d8cb674637f0fc41d9a7a9c8dede1082a3
ruhui <1170197422@qq.com>
2019-11-26 10:58:09 +0800

    读取 excel 中文件地址下载文件

--- common-utils/src/main/java/com/rh/commonutils/ExcelUtil.java
+++ common-utils/src/main/java/com/rh/commonutils/ExcelUtil.java
@@ -3,13 +3,17 @@ package com.rh.commonutils;
 import org.apache.poi.hssf.usermodel.*;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.CellRangeAddress;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.URLEncoder;
+import java.io.*;
+import java.net.*;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -115,4 +119,132 @@ public class ExcelUtil {
         }
 
     }
+
+    /**
+     *
+     * @Title: readXlsx
+     * @Description: 处理Xlsx文件
+     * @param @param path
+     * @param @return
+     * @param @throws Exception    设定文件
+     * @return List<List<String>>    返回类型
+     * @throws
+     */
+    public static List<List<String>> readXlsx(String path, String dirPath) throws Exception {
+        InputStream is = new FileInputStream(path);
+
+        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(is);
+        List<List<String>> result = new ArrayList<>();
+        // 循环每一页，并处理当前循环页
+        for (Sheet sheet : xssfWorkbook) {
+            XSSFSheet xssfSheet = (XSSFSheet) sheet;
+            if (xssfSheet == null) {
+                continue;
+            }
+            // 处理当前页，循环读取每一行
+            for (int rowNum = 1; rowNum <= xssfSheet.getLastRowNum(); rowNum++) {
+                XSSFRow xssfRow = xssfSheet.getRow(rowNum);
+                int minColIx = xssfRow.getFirstCellNum();
+                int maxColIx = xssfRow.getLastCellNum();
+                List<String> rowList = new ArrayList<>();
+                File file = null;
+                for (int colIx = minColIx; colIx < maxColIx; colIx++) {
+                    XSSFCell cell = xssfRow.getCell(colIx);
+                    if (cell == null) {
+                        continue;
+                    }
+                    if (colIx == 0) {
+                        file = new File(dirPath + cell.getStringCellValue());
+                        if (file.exists()) {
+                            continue;
+                        }
+
+                    }
+                    if (colIx == 1) {
+                        XSSFCell cell2 = xssfRow.getCell(colIx + 1);
+                        downloadFile(cell.getStringCellValue(), file.getAbsolutePath(), cell2.getStringCellValue());
+                    }
+                    rowList.add(cell.toString());
+                }
+                result.add(rowList);
+            }
+        }
+        return result;
+    }
+
+
+    /**
+     * 说明：根据指定URL将文件下载到指定目标位置
+     *
+     * @param urlPath
+     *            下载路径
+     * @param downloadDir
+     *            文件存放目录
+     * @return 返回下载文件
+     */
+    @SuppressWarnings("finally")
+    public static File downloadFile(String urlPath, String downloadDir, String fileName) {
+        File file = null;
+        try {
+            // 统一资源
+            URL url = new URL(urlPath);
+            // 连接类的父类，抽象类
+            URLConnection urlConnection = url.openConnection();
+            // http的连接类
+            HttpURLConnection httpURLConnection = (HttpURLConnection) urlConnection;
+            //设置超时
+            httpURLConnection.setConnectTimeout(1000*5);
+            //设置请求方式，默认是GET
+            // TODO
+//            httpURLConnection.setRequestMethod("POST");
+            // 设置字符编码
+            httpURLConnection.setRequestProperty("Charset", "UTF-8");
+            // 打开到此 URL引用的资源的通信链接（如果尚未建立这样的连接）。
+            httpURLConnection.connect();
+            // 文件大小
+            int fileLength = httpURLConnection.getContentLength();
+
+            // 控制台打印文件大小
+            System.out.println("您要下载的文件大小为:" + fileLength / 1024 + "KB");
+//            System.out.println("您要下载的文件大小为:" + fileLength / (1024 * 1024) + "MB");
+
+            // 建立链接从请求中获取数据
+//            URLConnection con = url.openConnection();
+            BufferedInputStream bin = new BufferedInputStream(httpURLConnection.getInputStream());
+            // 指定文件名称(有需求可以自定义)
+            // 指定存放位置(有需求可以自定义)
+            String path = downloadDir + File.separatorChar + fileName + ".pdf";
+            file = new File(path);
+            // 校验文件夹目录是否存在，不存在就创建一个目录
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+
+            OutputStream out = new FileOutputStream(file);
+            int size = 0;
+            int len = 0;
+            byte[] buf = new byte[2048];
+            while ((size = bin.read(buf)) != -1) {
+                len += size;
+                out.write(buf, 0, size);
+                // 控制台打印文件下载的百分比情况
+//                System.out.println("下载了-------> " + len * 100 / fileLength + "%\n");
+            }
+            // 关闭资源
+            bin.close();
+            out.close();
+            System.out.println("文件下载成功！");
+        } catch (MalformedURLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            System.out.println("文件下载失败！");
+        } finally {
+            return file;
+        }
+
+    }
+
 }
---
+++ common-utils/src/main/java/com/rh/commonutils/fileutils/ReadExcelCreateFile.java
@@ -0,0 +1,23 @@
+package com.rh.commonutils.fileutils;
+
+import com.alibaba.fastjson.JSON;
+import com.rh.commonutils.ExcelUtil;
+
+import java.util.List;
+
+/**
+ * content:
+ *
+ * @author Ruh
+ * @time 2019/10/28
+ **/
+public class ReadExcelCreateFile {
+    public static void main(String[] args) throws Exception {
+        String path = "E:\\ruhui\\working\\文档\\需求\\浦发对接\\文件拷贝\\10错误文件\\";
+        List<List<String>> lists = ExcelUtil.readXlsx(path.concat("浦发订单合同下载地址.xlsx"), path);
+
+//        String path = "E:\\ruhui\\working\\文档\\需求\\浦发对接\\文件拷贝\\错误文件详情\\";
+//        List<List<String>> lists = ExcelUtil.readXlsx("E:\\ruhui\\working\\文档\\需求\\浦发对接\\文件拷贝\\错误文件详情\\浦发订单错误合同下载地址.xlsx", path);
+        System.out.println(JSON.toJSONString(lists));
+    }
+}
